dd563d6f7fcebafaa57218d1c871c89b804158c5
kirill-gbn <papayok@mail.ru>
2023-12-19 17:19:19 +0400

    feat: add json file

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 const productBtn = document.querySelectorAll('.product-btn');
 productBtn.forEach(e=>e.addEventListener('click', event => {
     event.target.style.backgroundColor = 'rgba(0, 168, 45, 1)';
-    event.target.innerText = `В корзине`;
+    event.target.innerText = 'В корзине';
   }));
 
 
 //Удаляем товар из корзины
 
 const cross = document.querySelectorAll('.close');
+
 
 function handleClick(e) {
     const currentCross = e.currentTarget;
@@ -19,3 +20,14 @@ function handleClick(e) {
 cross.forEach(close => {
     close.addEventListener('click', handleClick)
 });
+
+
+
+
+// let cartItems = document.querySelector('.cart_items');
+
+// if (cartItems.length == 0) {
+//     document.location.replace("http://127.0.0.1:5500/online-store/index.html")
+    
+// };
+
